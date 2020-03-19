e33f6c26eafd00cc0e2b564c078c18d8c7d7d6db
Matt Green <verde.mateo.a@gmail.com>
2020-03-18 20:00:21 -0400

    Completes Section 3

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -23,7 +23,7 @@ const typeDefs = gql`
 
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
@@ -33,9 +33,29 @@ const typeDefs = gql`
 
   #You define your data graph's supported queries as fields of a special type called the Query type.
   type Query {
-    launches: [Launch]!
+    launches( # replace the current launches query with this one.
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
     me: User
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
 
   #Queries enable clients to fetch data, but not to modify data. To enable clients to modify data, our schema needs to define some mutations.
