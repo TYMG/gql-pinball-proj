28621da3a8f94b8506ebb1bdb2e3aac869312f6f
Matt Green <verde.mateo.a@gmail.com>
2020-03-18 21:35:20 -0400

    Completes Section 5

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,11 +1,27 @@
-import React, { Fragment }  from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { Fragment } from "react";
+import { RouteComponentProps } from "@reach/router";
+import gql from "graphql-tag";
 
 interface LaunchesProps extends RouteComponentProps {}
 
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    __typename
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
+
 const Launches: React.FC<LaunchesProps> = () => {
   return <div />;
-}
+};
 
 export default Launches;
-
