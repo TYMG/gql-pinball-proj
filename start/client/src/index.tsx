3441b8cc500c99058eaebfbad84fd2863a842419
Matt Green <verde.mateo.a@gmail.com>
2020-03-20 18:49:54 -0400

    First PT Commit

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -2,10 +2,11 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import gql from "graphql-tag";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 import React from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
+import Login from "./pages/login";
 import injectStyles from "./styles";
 
 const cache = new InMemoryCache();
@@ -15,13 +16,37 @@ const link = new HttpLink({
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
-  link
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql",
+
+    headers: {
+      authorization: localStorage.getItem("token")
+    }
+  })
 });
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem("token"),
+    cartItems: []
+  }
+});
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById("root")
 );
